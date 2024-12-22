5dbce41becbd195a255464df6ae1828d223552d8
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-12-22 22:07:33 +0330

    Remove coments

--- TransportAgency/cmd/main.go
+++ TransportAgency/cmd/main.go
@@ -8,27 +8,22 @@ import (
 )
 
 func main() {
-	// Initialize the repository and service
 	agencyRepo := adapter.NewInMemoryAgencyRepo()
 	createAgencyService := &service.CreateAgencyService{
 		Repo: agencyRepo,
 	}
 
-	// Create a new Fiber app
 	app := fiber.New()
 
-	// Define the HTTP handler for creating an agency
 	app.Post("/create-agency", func(c *fiber.Ctx) error {
 		var command service.CreateAgencyCommand
 
-		// Parse the request body into CreateAgencyCommand
 		if err := c.BodyParser(&command); err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 				"error": "Invalid input",
 			})
 		}
 
-		// Execute the service to create the agency
 		agencyID, err := createAgencyService.Execute(command)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -36,13 +31,11 @@ func main() {
 			})
 		}
 
-		// Send the response back with the created agency ID
 		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 			"id": agencyID,
 		})
 	})
 
-	// Start the server
 	log.Println("Starting server on :8080")
 	if err := app.Listen(":8080"); err != nil {
 		log.Fatal("Error starting server: ", err)
