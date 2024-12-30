77ea4262cf5d8ea7cc2e8cc3948da1ad8b75dadf
SinaKarimi <Sinakarimi0938@gmaiol.com>
2024-12-30 23:36:21 +0330

    Added json tag to our types

--- payment/pkg/adapters/storage/types/history.go
+++ payment/pkg/adapters/storage/types/history.go
@@ -8,15 +8,15 @@ import (
 
 type History struct {
 	gorm.Model
-	Id          uint //HistoryId
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
-	Code        uuid.UUID
-	IsApproved  bool
-	Price       float64
-	Source      uint //domain.WalletID
-	Destination uint //domain.WalletID
-	Title       string
-	Description string
+	Id          uint      `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
+	Code        uuid.UUID `json:"code"`
+	IsApproved  bool      `json:"is_approved"`
+	Price       float64   `json:"price"`
+	Source      uint      `json:"source"`
+	Destination uint      `json:"destination"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
 }
